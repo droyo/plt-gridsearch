974f9da705fdb659cb26b1685bde83c8a1a013bd
David Arroyo <david.a.arroyo@gmail.com>
2009-10-27 02:44:28 +0900

    Adapted Jim's GUI code to be a more spartan interface. Now there is nothing but the display and some controls for start/stop and graph generation/step speed. Nothing is hooked up yet, of course, but the general layout is something I'd like to stick to.

--- graph-gui.scm
+++ graph-gui.scm
@@ -1,113 +1,53 @@
+;;;; Notes on the GUI: I want it to be very spartan, ideally a huge
+;;;; canvas area with buttons and a slider along the top and
+;;;; bottom. Text output can be given in the repl we run the gui
+;;;; from. This is not meant to be a standalone application.
 #lang scheme/gui
 
-(require "graph.scm")
-; The GUI for the Grid program
+;(require "graph.scm" "graph-layout.scm")
 
-; Creates the frame for the GUI, labels it
-(define frame (new frame% [label "Graph Search"]
-                   [width 1000]
-                   [height 1000]))
+;; Our main window
+(define *top-frame*
+  (new frame%
+       [label "Graph Search"]
+       [min-width 320]
+       [min-height 200]))
 
-;Creates the Menu Bar
-(define menu-bar
-  (new menu-bar% [parent frame]))
+;; The graph area
+(define *graph-canvas*
+  (new canvas% [parent *top-frame*]))
 
-; Creates the Menu Items
-(new menu% [label "File"]	 
-     [parent menu-bar])
-(new menu% [label "Edit"]	 
-     [parent menu-bar])
-(new menu% [label "View"]	 
-     [parent menu-bar])
-(new menu% [label "Options"]	 
-     [parent menu-bar])
-
-
-(define grid (square-grid 8))
-(define layout (force-layout grid .0003 10000 .4 .99))
-; Define the procedure that draws the grid
-(define (draw-grid dc)
-  (let ((points (map (lambda (p)
-                       (list (* (car p) 1000)
-                             (* (cadr p) 500)))
-                     layout))
-        (draw-node (lambda (pt)
-                     (send dc draw-rectangle
-                           (car pt)
-                           (cadr pt)
-                           40 40))))
-    (for-each draw-node points)))
-
-;Define the Grid area
-(define canvas
-  (new canvas% [parent frame]
-       [min-width 1000]
-       [min-height 500]
-       [paint-callback (lambda (canvas dc) (draw-grid dc))]))
-
-; Get the canvas's drawing context
-(define dc (send canvas get-dc))
-
-; Make some pens and brushes
-(define no-pen (make-object pen% "BLACK" 1 'transparent))
-(define no-brush (make-object brush% "BLACK" 'transparent))
-(define blue-brush (make-object brush% "BLUE" 'solid))
-(define black-brush (make-object brush% "BLACK" 'solid))
-(define white-brush (make-object brush% "WHITE" 'solid))
-(define gray-brush (make-object brush% "GRAY" 'solid))
-(define yellow-brush (make-object brush% "YELLOW" 'solid))
-(define red-pen (make-object pen% "RED" 2 'solid))
-
-;Make a panel to hold the message
-(define vertpanel (new horizontal-panel% [parent frame]
-                       [alignment '(center center)]))
-
-; Defines a sample message
-(define msg (new message% [parent vertpanel]
-                 [label "THIS IS THE GRAPH / GRID THING"]))
-
-; Make a panel to hold the buttons
-  (define panel (new horizontal-panel% [parent frame]
-                     [alignment '(center center)]))
+;; A horizontal bar of buttons and a slider
+(define *button-bar* (new horizontal-panel%
+			  [parent *top-frame*]
+			  [alignment '(center bottom)]))
   
-; Make some buttons in the panel
-(new button% [parent panel]
-     [label "Start"]
-    
-     ; Callback procedure for a button click:
-     (callback (lambda (button event)
-                 (send msg set-label "GRAPHGRAPHGRAPH!"))))
+;; Make some buttons in the panel
+(define (create-buttons)
+  ;; Play/Pause
+  (new button%
+       [parent *button-bar*]
+       [label "Play"])
+  (new button%
+       [parent *button-bar*]
+       [label "Pause"])
+  ;; Slider bar
+  (new slider%
+       [label "Speed"]
+       [parent *button-bar*]
+       [min-value 0]
+       [max-value 1000])
+  ;; Prev/Next buttons
+  (new button%
+       [parent *button-bar*]
+       [label "<"])
+  (new button%
+       [parent *button-bar*]
+       [label ">"]))
 
-(new button% [parent panel]
-     [label "Stop"]
-     
-     ; Callback procedure for a button click:
-     (callback (lambda (button event)
-                 (send msg set-label "STOPSTOPSTOP!"))))
+(create-buttons)
 
-; Defines the interaction window (IW) panel
-(define iw
-  (new panel% [parent frame]
-       [min-width 400]
-       [min-height 100]
-       [border 5]))
-
-; Creates the IW message
-(new message% 
-     [label "This is where the text for the Interaction Window will, go, line by line, and can detail the progrss and debug information as the program runs.
-Line 2
-Line 3
-Line 4
-Line 5
-Etc."]
-     [parent iw])
-
-
-; Shows the frame
-(send frame show #t)
-
-; Wait a second to let the window get ready
-(sleep/yield 1)
-
-; Draw the grid
-(draw-grid dc)
+(define (run)
+  (send *top-frame* show #t);Shows the frame
+  ;; Wait a second to let the window get ready
+  (sleep/yield 1))
