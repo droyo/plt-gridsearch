8e3b4badc28ae6b6bf3de37ed4524bd8e96baad9
David Arroyo <david.a.arroyo@gmail.com>
2009-10-27 15:31:34 +0900

    Added some refinements, the next/prev buttons now work and generate new graphs when needed. The Start/Stop and slider do not work yet, that requires an event loop.

--- graph-gui.scm
+++ graph-gui.scm
@@ -4,10 +4,34 @@
 ;;;; from. This is not meant to be a standalone application.
 #lang scheme/gui
 
-(require srfi/1 "graph.scm" "graph-layout.scm" "vector-operations.scm")
+(require srfi/1
+	 "graph.scm"
+	 "graph-layout.scm"
+	 "vector-operations.scm")
+
+(define-syntax flip!
+  (syntax-rules ()
+    ((_ var)
+     (begin (set! var (not var))
+	    var))))
+
+;; Add an item to the end of a list, return its index
+(define-syntax add!
+  (syntax-rules ()
+    ((_ item list-var)
+     (begin (set! list-var
+		  (append list-var (list item)))
+	    (length list-var)))))
+
+(define-syntax inc!
+  (syntax-rules ()
+    ((_ var x)
+     (begin (set! var (+ var x))
+	    var))
+    ((_ var)
+     (inc! var 1))))
 
 (define *node-size* 30)
-
 (define (draw-node dc name x y)
   (let ((stroke (make-object pen% "BLACK" 2 'solid)))
     (send dc set-pen stroke)
@@ -21,7 +45,7 @@
 
 ;; Only undirected edges for now.
 (define (draw-edge dc from to)
-  (let ((stroke (make-object pen% "BLACK" 2 'solid)))
+  (let ((stroke (make-object pen% "GREY" 2 'solid)))
     (send dc draw-line (car from) (cadr from)
 	  (car to) (cadr to))))
 
@@ -48,58 +72,110 @@
 		(apply draw-node dc node position))
 	      (vertices graph) points)))
 
+;; A canvas that grows to fill its container
+(define (create-canvas par callback)
+  (new canvas%
+       [parent par]
+       [style '(border)]
+       [stretchable-width #t]
+       [stretchable-height #t]
+       [paint-callback callback]))
+
+;; A closure that makes buttons for a container
+(define (button-maker par)
+  (lambda (name fun)
+    (new button%
+	 [parent par]
+	 [label name]
+	 [callback fun])))
+
+;; A horizontal bar container
+(define (create-bar par)
+  (new horizontal-panel%
+       [parent par]
+       [alignment '(center bottom)]
+       [stretchable-height #f]))
+
+(define (create-window)
+  (new frame%
+       [label "Graph Search"]
+       [min-width 320]
+       [min-height 200]))
+
+(define (create-panel par)
+  (new vertical-panel%
+       [parent par]
+       [alignment '(center top)]))
+
+(define (create-slider par fun)
+  (new slider%
+       [label ""]
+       [parent par]
+       [min-value 1]
+       [max-value 1000]
+       [callback fun]))
+
+;; We'll manage a list of graphs that the user can cycle through using
+;; the <> buttons
+(define (graph-list)
+  (list (new-graph)))
+
+(define *layout-function* random-layout)
+(define (new-graph)
+  (let ((g (random-graph 8 .3)))
+    (list g (*layout-function* (vertices g)))))
+
+;; Where the action's at
 (define (run-grid-gui)
-  (let* ((graph (random-graph 6 .4))
-	 (layout (random-layout (vertices graph)))
-	 (win (new frame%
-		   [label "Graph Search"]
-		   [min-width 320]
-		   [min-height 200]))
-	 (panel (new vertical-panel%
-		     [parent win]
-		     [alignment '(center top)]))
-	 (canvas (new canvas%
-		      [parent panel]
-		      [style '(border)]
-		      [stretchable-width #t]
-		      [stretchable-height #t]
-		      [paint-callback
-		       (lambda (canvas dc)
-			 (draw-graph graph
-				     layout
-				     canvas
-				     dc))]))
-	 (bar (new horizontal-panel%
-		   [parent panel]
-		   [alignment '(center bottom)]
-		   [stretchable-height #f]))
-	 (make-button (lambda (name fun)
-			(new button%
-			     [parent bar]
-			     [label name]
-			     [callback fun])))
+  (let* ((graphs (graph-list))
+	 (current-graph 0)
+	 ;; Keep our graphs in a list and add as we need them
+	 (to-graph
+	  (lambda (x)
+	    (cond
+	     ((>= (+ current-graph x) (length graphs))
+	      (add! (new-graph) graphs)
+	      (list-ref graphs (inc! current-graph)))
+	     ((> (length graphs) (+ current-graph x) -1)
+	      (list-ref graphs (inc! current-graph x)))
+	     (else
+	      (list-ref graphs current-graph)))))
+	 
+	 ;; Our gui elements
+	 (win (create-window))
+	 (panel (create-panel win))
+	 (canvas (create-canvas
+		  panel
+		  (lambda (c dc)
+		    (send dc clear)
+		    (let ((g (to-graph 0)))
+		      (draw-graph (car g) (cadr g)
+				  c dc)))))
+	 (bar (create-bar panel))
+	 (make-button (button-maker bar))
+
+	 (start (make-button
+		 "Start" (lambda (b e)
+			   (if (flip! running)
+			       (send b set-label "Stop")
+			       (send b set-label "Start")))))
+	 (slider (create-slider
+		  bar (lambda (b e)
+			(set! tempo (send b get-value)))))
+
+	 (prev (make-button
+		"<" (lambda (b e)
+		      (to-graph -1)
+		      (send b show (> current-graph 0))
+		      (send canvas refresh))))
+	 (next (make-button
+		">" (lambda (b e)
+		      (to-graph +1)
+		      (send prev show (> current-graph 0))
+		      (send canvas refresh))))
 	 (tempo 1)
 	 (running #f))
 
-    (make-button "Start"
-		 (lambda (b e)
-		   (set! running (not running))
-		   (if running
-		       (send b set-label "Stop")
-		       (send b set-label "Start"))))
-    
-    (new slider%
-	 [label ""]
-	 [parent bar]
-	 [min-value 1]
-	 [max-value 1000]
-	 [callback (lambda (b e)
-		     (set! tempo
-			   (send b get-value))
-		     (printf "Tempo: ~A~%" tempo))])
-
-    (make-button "<" void)
-    (make-button ">" void)
-
+    ;; [Start/Stop] [---|--------][<][>]
     (send win show #t)
     (sleep/yield 1)))
